927e4b0e82aa16aab6e98f120bfbc182d4f39d5a
Rose Hudson <cv21874@bristol.ac.uk>
2023-01-19 15:49:33 +0000

    initial commit with VA bindgen setup

---
+++ va-sys/build.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=va");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to write bindings");
+}
